d1e2ce328b99be382ec026f06a207143c65227bd
amrit.talwar <amrit.talwar@wonderbill.com>
2020-06-05 14:07:57 +0100

    Add GET and PATCH endpoints for task/:id + add Task.dto file

--- backend/src/Tasks/Task.controller.ts
+++ backend/src/Tasks/Task.controller.ts
@@ -1,11 +1,14 @@
 import express from "express";
 import { Task } from "./Task.entity";
 import { TaskStatus } from "./TaskStatus.enum";
+import { validateDto } from "../middlewares/validateDto";
+import { updateTaskDto, createTaskDto } from "./Task.dto";
 
 const TasksController: express.Router = express.Router();
 
 TasksController.post(
   "/create",
+  validateDto(createTaskDto),
   async (
     req: express.Request,
     res: express.Response,
@@ -23,11 +26,42 @@ TasksController.post(
   },
 );
 
-TasksController.patch(
+TasksController.get(
   "/:id",
-  async (req: express.Request, res: express.Response) => {
-    console.log("ping");
+  async (
+    req: express.Request,
+    res: express.Response,
+  ): Promise<express.Response> => {
+    const task: Task = await getTaskById(parseInt(req.params.id));
+    return res.status(200).json(task);
   },
 );
+
+TasksController.patch(
+  "/:id",
+  validateDto(updateTaskDto),
+  async (
+    req: express.Request,
+    res: express.Response,
+  ): Promise<express.Response> => {
+    const task: Task = await getTaskById(parseInt(req.params.id));
+    if (!task) {
+      return res.status(404).send();
+    }
+
+    Object.keys(req.body).forEach((key) => {
+      task[key] = req.body[key];
+    });
+    await task.save();
+
+    return res.status(200).json(task);
+  },
+);
+
+const getTaskById = async (taskId: number): Promise<Task> => {
+  return await Task.findOne({
+    where: { id: taskId },
+  });
+};
 
 export { TasksController };
