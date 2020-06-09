ef5e1b95111d00b5d4316baf5a7ad99981db4325
amrit.talwar <amrit.talwar@wonderbill.com>
2020-06-09 15:38:12 +0100

    Remove custom types folder (not needed after removing jwt middleware)

--- backend/@types/express/index.d.ts
+++
@@ -1,9 +0,0 @@
-import { jwtDecodeInterface } from "../../src/middlewares/verifyJwtToken";
-
-declare global {
-  namespace Express {
-    interface Request {
-      jwtDecode: jwtDecodeInterface;
-    }
-  }
-}
